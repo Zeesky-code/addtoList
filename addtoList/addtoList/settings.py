4317f3498aaa8590c22ccd94670d002efd3d3b54
Zainab Lawal <zainablaw1012@gmail.com>
2022-07-08 16:10:30 +0300

    Created home page

--- addtoList/addtoList/settings.py
+++ addtoList/addtoList/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_DIR=os.path.join(BASE_DIR,'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'addtoList',
 ]
 
 MIDDLEWARE = [
@@ -104,8 +105,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS=[STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
