838a5b512a9f797f8f73b31ea5714ea3a9ccaea1
Zainab Lawal <zainablaw1012@gmail.com>
2022-09-04 14:47:08 +0300

    Deployment to Render

--- addtoList/settings.py
+++ addtoList/settings.py
@@ -2,6 +2,7 @@ import os
 import django_heroku
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -13,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', "default_value")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG',default=True,cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
@@ -110,8 +111,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR=os.path.join(BASE_DIR,'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if not DEBUG:    
+
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = (
