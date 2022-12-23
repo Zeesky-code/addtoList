788eb6f45fa3c89147f6ccbc1fa285cc39df6407
Zainab Lawal <zainablaw1012@gmail.com>
2022-12-23 21:01:01 +0300

    Update settings.py
    test

--- addtoList/settings.py
+++ addtoList/settings.py
@@ -112,8 +112,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 if not DEBUG:    
-
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
